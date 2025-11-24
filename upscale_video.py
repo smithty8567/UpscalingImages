ddc3ccdd79a63f93d8de28fe24ddd33c63e66a23
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-11-23 22:42:57 -0600

    Downscaling

--- upscale_video.py
+++ upscale_video.py
@@ -25,6 +25,7 @@ def live(size, device, flip=True, path = 0):
   h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * 4)
   fps = cap.get(cv2.CAP_PROP_FPS)
   writer = cv2.VideoWriter('output.mp4', -1, fps, (h, w * 2))
+  downscale_factor = 4
 
   if not cap.isOpened():
     print("Could not open webcam")
@@ -37,8 +38,17 @@ def live(size, device, flip=True, path = 0):
     if not ret: break
     frame = frame.astype(np.float32) / 255
     if flip: frame = frame[:,::-1]
-    inp = frame
-    inp_img = inp
+    inp = cv2.resize(
+      frame,
+      (h // downscale_factor, w // downscale_factor),
+      interpolation=cv2.INTER_CUBIC
+    )
+    inp_img = cv2.resize(
+      inp,
+      (h, w),
+      interpolation=cv2.INTER_CUBIC
+    )
+
     # hight, width, _  = frame.shape
     # if hight < width:
     #   diff = (width - hight)//2
@@ -46,29 +56,24 @@ def live(size, device, flip=True, path = 0):
     # if hight > width:
     #   diff = (hight - width)//2
     #   frame = frame[diff:hight-diff, :]
-    inp = cv2.resize(
-      frame,
-      (size, size),
-      interpolation=cv2.INTER_NEAREST
-    )
+    # inp = cv2.resize(
+    #   frame,
+    #   (size, size),
+    #   interpolation=cv2.INTER_NEAREST
+    # )
     # inp_img = cv2.resize(
     #   inp,
     #   (h, w),
     #   interpolation=cv2.INTER_NEAREST
     # )
+
     inp = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
     inp = torch.tensor(inp, dtype=torch.float32).permute(2,0,1)
     inp = inp.unsqueeze(0).to(device)  # [1,3,H,W]
-    with torch.no_grad():
-      out = model(inp)  # [1,3,H,W]
+    with torch.no_grad(): out = model(inp)  # [1,3,H,W]
     out_img = out.squeeze().cpu().clamp(0, 1).permute(1,2,0)
     out_img = cv2.cvtColor(np.array(out_img), cv2.COLOR_RGB2BGR)
-    pixelated = cv2.resize(
-      out_img,
-      (h, w),
-      interpolation=cv2.INTER_NEAREST
-    )
-    combined = np.hstack((inp_img, pixelated))
+    combined = np.hstack((inp_img, out_img))
     writer.write(combined)
     cv2.imshow("Camera (left)  |  Model Output (right)", combined)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
